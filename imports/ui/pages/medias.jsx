e0bf9e5743bc97679a22018a9a6a2b62ca3eb916
LucasPadovan <zerobba8@hotmail.com>
2016-09-24 02:38:57 -0300

    Medias form component pretty basic

--- imports/ui/pages/medias.jsx
+++ imports/ui/pages/medias.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { MediaForm } from '../components/medias/Form.jsx';
 import { MediaList } from '../components/medias/List.jsx';
 
 
 export const Medias = () => (
   <div>
     <h2 class="l-pad-top-1">Lista de series, animes, peliculas y juegos</h2>
+
+    <MediaForm />
 
     <div class="l-pad-top-1"><MediaList /></div>
   </div>
